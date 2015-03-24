8c6c61885579dfbc61b3e2407e481241c7f7c209
chessman1 <chess_man@abv.bg>
2015-03-24 17:54:49 +0200

    first comment

---
+++ sayhello.js
@@ -0,0 +1,8 @@
+function sum(numbers) {
+	var sum=0;
+	for(var i=1;i<=numbers;++i) {
+		sum+=i;
+	}
+	return sum;
+}
+
---
+++ sayhello_test.js
@@ -0,0 +1,6 @@
+describe("sayhello", function () {
+	it("should give 15" + " result is 15", function(){
+		expect(sum(5)).toEqual(15);
+	});
+});
+
